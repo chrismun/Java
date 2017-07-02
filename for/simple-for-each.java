604e305de84273025f045e594714c12dfec4851d
chrismunley <chrismunley1@gmail.com>
2017-07-01 22:02:11 -0400

    Create simple-for-each.java

---
+++ for/simple-for-each.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+class ForEach{  
+  public static void main(String args[]){  
+   ArrayList<String> list = new ArrayList<String>();  
+   list.add("Print");  
+   list.add("Every");  
+   list.add("String");  
+  
+   for(String s:list){  
+     System.out.println(s);  
+   }  
+ }   
+}  
