9fa242d6bc6ba9c72ded78a44252c06d38052343
Chris Munley <chrismunley1@gmail.com>
2017-07-10 10:01:13 -0400

    Updated

--- for/simple-for-each.java
+++ for/simple-for-each.java
@@ -1,13 +1,15 @@
-import java.util.*;  
-class ForEach{  
-  public static void main(String args[]){  
-   ArrayList<String> list = new ArrayList<String>();  
-   list.add("Print");  
-   list.add("Every");  
-   list.add("String");  
-  
-   for(String s:list){  
-     System.out.println(s);  
-   }  
- }   
-}  
+import java.util.*;
+class ForEach{
+  public static void main(String args[]){
+   ArrayList<String> list = new ArrayList<String>();
+   list.add("Print");
+   list.add("Every");
+   list.add("String");
+
+  //enhanced for loop executes the code inside the loop once for each object in what you iterate over (list in this case)
+  //The value of your iterator is equal to the object that the current iteration uses
+   for(String s:list){
+     System.out.println(s);
+   }
+ }
+}
