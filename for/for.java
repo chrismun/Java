5f8e7ba913497a5c0fbf61ab2b6dedbaa189886a
chrismunley <chrismunley1@gmail.com>
2017-07-09 22:10:59 -0400

    Create for.java

---
+++ for/for.java
@@ -0,0 +1,15 @@
+class main {
+  public static void main(String[] args) {
+    
+    for(int i = 0; i < 100; i++) { 
+    
+    /* 
+    "int i = 0" is the initialization of the count variable. 
+    "i < 100" is what determines when to stop looping. 
+    "i++" is executed at the end of every loop iteration, increasing i by 1.
+    */
+    
+    System.out.println(i); // prints out every number on a new line (because of println), from 1 to 99 (not including 100)
+    }
+  }
+}
