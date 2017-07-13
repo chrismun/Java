377103557a01c3ddbd23980c35f26d9b0d69c899
chrismunley <chrismunley1@gmail.com>
2017-07-13 10:08:02 -0400

    Update MassCalc.java

--- MassCalc.java
+++ MassCalc.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 class Main {
+  
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("What planet are you on?");
@@ -7,7 +8,6 @@ class Main {
     System.out.println("What is your mass on earth, in kilograms?");
     double mass = scan.nextInt();
     calculateMass(planet, mass);
-    
   }
   
  public static void calculateMass(String planet, double mass) {
@@ -55,8 +55,6 @@ class Main {
       }
       System.out.print(returnState);
    }
-   
-   
  }
 
 
