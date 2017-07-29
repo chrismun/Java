0b7065f52ab98fca35334a2952c0bf9ee98aaed3
chrismunley <chrismunley1@gmail.com>
2017-07-29 18:45:36 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -29,20 +29,20 @@ System.out.println("Enter the first number");
 int x = scan.nextInt();
 System.out.println("Enter the second number");
 int y = scan.nextInt();
-System.out.println("Which operation would you like to execute? (x*y x/y x+y x-y)");
-System.out.println("1: Multiplication, 2: Division, 3: Addition, 4: Subtraction   (type just 1 2 3 or 4");
-int z = scan.nextInt();
-if(int z==1){
-  System.out.println("The product is: " + Multiplication(x,y));
+System.out.println("Which operation would you like to do? (x*y x/y x+y x-y)");
+System.out.println("1: Multiplication, 2: Division, 3: Addition, 4: Subtraction   (type 1 2 3 or 4");
+int input = scan.nextInt().trim();
+if(int input==1){
+  System.out.println(x + " * " + y + "=" + Multiply(x,y));
 }
-else if (int z==2) {
-  System.out.println("The quotient is: " + Division(x,y));
+else if (int input==2) {
+  System.out.println((x + " / " + y + "=" + Divide(x,y));
 }
-else if (int z==3) {
-  System.out.println("The sum is: " + Addition(x,y));
+else if (int input==3) {
+  System.out.println((x + " + " + y + "=" + Add(x,y));
 }
-else if (int z==4) {
-  System.out.println("The difference is: " + Subtraction(x,y));
+else if (int input==4) {
+  System.out.println((x + " - " + y + "=" + Subtract(x,y));
 }                 
 }
 }
