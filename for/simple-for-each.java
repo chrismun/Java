7ef7823916107279c37f06910c39f4ae285d6fc7
chrismunley <chrismunley1@gmail.com>
2017-07-31 15:35:09 -0400

    Update simple-for-each.java

--- for/simple-for-each.java
+++ for/simple-for-each.java
@@ -2,12 +2,13 @@ import java.util.*;
 class ForEach{
   public static void main(String args[]){
    ArrayList<String> list = new ArrayList<String>();
-   list.add("Print");
-   list.add("Every");
+   list.add("Print ");
+   list.add("Every ");
    list.add("String");
 
   //enhanced for loop executes the code inside the loop once for each object in what you iterate over (list in this case)
   //The value of your iterator is equal to the object that the current iteration uses
+  //this loop will print "Print Every String" with each word on a new line
    for(String s:list){
      System.out.println(s);
    }
