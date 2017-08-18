337d50749fb3ac420870365339fa122895aaf9f5
chrismunley <chrismunley1@gmail.com>
2017-08-17 23:01:43 -0400

    Create write.java

---
+++ write.java
@@ -0,0 +1,30 @@
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.File;
+
+import java.util.Random;
+
+/*
+    Writes 100 random integers to randomInts.txt
+*/
+
+public class Write
+{	public static void main(String[] args)
+	{	
+    try {	
+    
+      PrintStream writer = new PrintStream( new File("randomInts.txt"));
+			Random random = new Random();
+			final int LIMIT = 100;
+
+			for(int i = 0; i < LIMIT; i++) {	
+        writer.println( random.nextInt() );
+			}
+      writer.close();
+      
+     } catch(IOException e) {	
+      System.out.println("An error occured while writing to the file");
+		}
+    
+	}
+}
